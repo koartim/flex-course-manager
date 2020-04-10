5b0216ac297c13c76312edf908721951b5ae446f
Tim Koar <koartimothy@gmail.com>
2020-04-10 16:21:42 -0400

    implemented auth login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import { useAuth0 } from "./react-auth0-spa";
+
 
 function App() {
+
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  // componentDidMount() {
+  //   fetch("courses.json")
+  //     .then(rsp => rsp.json())
+  //     .then(data => console.log(data))
+  // }
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Navbar/>
       </header>
     </div>
   );
