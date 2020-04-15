9928217f15b27f3e5c71e6b8c55b29989a339f32
Tim Koar <koartimothy@gmail.com>
2020-04-15 12:31:57 -0400

    added form validations

--- src/components/CreateCourse.js
+++ src/components/CreateCourse.js
@@ -8,7 +8,10 @@ class CreateCourse extends React.Component {
         difficulty: "",
         img_url: "",
         description: "",
-        id: ""
+        id: "",
+        instructor_email: "",
+        message: "",
+        textStyle: ""
     }
 
     handleChange = (e) => {
@@ -18,7 +21,18 @@ class CreateCourse extends React.Component {
     };
 
     handleSubmit = async (e) => {
-        e.preventDefault();
+       e.preventDefault();
+       const validateEmail = (instructor_email) => {
+         let re = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i;
+         return re.test(instructor_email);
+       }
+       if (this.state.instructor_email === "" || !validateEmail(this.state.instructor_email)) {
+         this.setState({message: "All fields are mandatory, and email must be valid", textStyle: "danger"});
+       } else {
+          if (validateEmail(this.state.instructor_email)) {
+            this.setState({message:"Looks good!", textStyle: "success"})
+          }
+       }
        await fetch("http://localhost:4000/courses/", {
            method: 'post',
            headers: {
@@ -28,12 +42,23 @@ class CreateCourse extends React.Component {
                name: this.state.name,
                difficulty: this.state.difficulty,
                img_url: this.state.img_url,
-               description: this.state.img_url
+               description: this.state.description,
+               instructor_email: this.state.instructor_email
            })
        }).then(rsp => rsp.json())
          .then(data => console.log(data))
          .catch(console.error()
          );
+         this.setState({
+           name: "",
+           difficulty: "",
+           img_url: "",
+           description: "",
+           id: "",
+           instructor_email: "",
+           message: "",
+           textStyle: ""
+         })
     }
 
     render() {
@@ -48,6 +73,9 @@ class CreateCourse extends React.Component {
                     <input placeholder="www.image_src.com" type="text" name="img_url"/>
                     <label htmlFor="description">description</label>
                     <input placeholder="brief bio of the course you want to add" type="text" name="description"/>
+                    <label htmlFor="instructor_email">instructor email</label>
+                    <input placeholder="instructor email" type="text" name="instructor_email"/>
+                    {this.state.message !== '' && <div className={`text text-${this.state.textStyle}`}>{this.state.message}</div>}
                     <button>Submit</button>
                 </form>
                 </div>
